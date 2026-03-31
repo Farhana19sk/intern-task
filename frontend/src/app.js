6e95677acd58ac45c6b2b8af5fbf584360e420b5
Farhana19sk <farhana.shaik.119@gmail.com>
2026-03-31 20:52:06 +0530

    Create main App component with Login and Register

---
+++ frontend/src/app.js
@@ -0,0 +1,16 @@
+import Login from "./Login";
+import Register from "./Register";
+
+function App() {
+  return (
+    <div>
+      <h1>Intern Task</h1>
+
+      <Register />
+      <hr />
+      <Login />
+    </div>
+  );
+}
+
+export default App;
